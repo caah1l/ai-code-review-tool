5706c3c13cd8f11cb74113a2be3b91d8a83feae4
Sahil Agrawal <agrawalsahil60@gmail.com>
2025-11-04 14:20:36 +0530

    saving reviews to DB wrt user

--- src/main/java/com/example/ai_code_review_tool/review/model/Review.java
+++ src/main/java/com/example/ai_code_review_tool/review/model/Review.java
@@ -1,4 +1,38 @@
 package com.example.ai_code_review_tool.review.model;
 
+import com.example.ai_code_review_tool.auth.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String prUrl;
+    private String summary;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String scopeOfImprovementJson;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String suggestedRefactorsJson;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- src/main/java/com/example/ai_code_review_tool/review/repository/ReviewRepository.java
+++ src/main/java/com/example/ai_code_review_tool/review/repository/ReviewRepository.java
@@ -1,4 +1,11 @@
 package com.example.ai_code_review_tool.review.repository;
 
-public class ReviewRepository {
+import com.example.ai_code_review_tool.auth.model.User;
+import com.example.ai_code_review_tool.review.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review,Long> {
+    List<Review> findByUser(User user);
 }
